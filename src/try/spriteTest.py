2e248ca74957e234b99f3a7ace180dc9295d3fa7
haizhiship <haizhiship@163.com>
2019-01-22 17:10:21 +0800

    一半的鼠标移动图案

--- src/try/spriteTest.py
+++ src/try/spriteTest.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from pygame.locals import *
 
+mouse_image_filename = '..\..\\resource\\fugu.png'
+
 class mySprite(pygame.sprite.Sprite):
     def __init__(self,color,initial_position):
         pygame.sprite.Sprite.__init__(self)
@@ -33,18 +35,45 @@ DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT),0,32)
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption('Hello Pygame World!')
 
-b=mySprite([255,0,0],[50,100])
-DISPLAYSURF.blit(b.image,b.rect)
+changeSurf=mySprite([255,0,0],[50,100])
+#changeImage = pygame.image.load(mouse_image_filename).convert_alpha()
+changeImage = changeSurf.image
+imagePos = []
+imageRect = []
 
+def judgeMousePosInRect(imagePos, imageRect):
+    x, y = pygame.mouse.get_pos()
+    if x > imagePos[0] + 5 and x <= imagePos[0]- 5 + imageRect[0] \
+        and y > imagePos[1] and y < imagePos[1] + imageRect[1]:
+        return 'center'
+    elif  x < imagePos[0] and x > imagePos[0] -5 \
+            and y > imagePos[1] and y < imagePos[1] + imageRect[1]:
+        return 'left'
+    elif x > imagePos[0] + imageRect[0] - 5 and x < imagePos[0] + imageRect[0] + 2 \
+        and y > imagePos[1] and y < imagePos[1] + imageRect[1]:
+        return 'right'
+    return ''
 
-
+canMove = False
 while True: # main game loop
+    DISPLAYSURF.fill(WHITE)
+    pos = judgeMousePosInRect([150,100], )
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        # 鼠标点击后，方块的大小会改变为(150,150)
         elif event.type == MOUSEBUTTONDOWN:
-            beginPos = pygame.mouse.get_pos()
+            canMove = True
+        elif event.type == MOUSEBUTTONUP:
+            canMove = False
+        elif event.type == MOUSEMOTION:
+            if canMove == True:
+                beginPos = pygame.mouse.get_pos()
+                changeImage = pygame.transform.scale(changeImage, (abs(beginPos[0]-150), 50))
 
 
+                print(beginPos[0] - 150)
+
+    DISPLAYSURF.blit(changeImage, (150, 100))
     pygame.display.update()
